7a2a7dfe27450a180d7d909e33ffc5c5b0af7920
hiepokbaby <125280452+hiepokbaby@users.noreply.github.com>
2023-03-06 22:47:29 +0700

    Add files via upload

--- baithuchanh2802/src/s55.java
+++ baithuchanh2802/src/s55.java
@@ -1,4 +1,16 @@
 
-public class s55 {
+public class Slide55 {
 
+	public static void main(String[] args) {
+
+		System.out.println("cac so chia het cho 5 :");
+		
+		for(int i=0;i<=100;i++)
+		{
+			if(i % 5 == 0) {
+				break;
+			}
+			System.out.println(i);
+		}	
+}
 }
